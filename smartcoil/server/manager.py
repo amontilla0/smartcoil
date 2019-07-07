d621e30588e99e08e02c586212631d4f91e65eee
Abraham R. Montilla Linares <amontilla0@gmail.com>
2019-07-06 20:28:13 -0400

    - Refactoring Flask server code to manage POST requests coming from a Smart Home Alexa Skill.

--- smartcoil/server/manager.py
+++ smartcoil/server/manager.py
@@ -1,52 +1,24 @@
-import logging
-import os
+from flask import Flask, request #import main Flask class and request object
 
-from flask import Flask
-from flask_ask import Ask, request, session, question, statement
-import RPi.GPIO as GPIO
+app = Flask(__name__) #create the Flask app
 
-app = Flask(__name__)
-ask = Ask(app, "/")
-logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+@app.route('/query-example')
+def query_example():
+    return 'Todo...'
 
-STATUSON = ['on','high', 'cool']
-STATUSOFF = ['off','low']
+@app.route('/form-example')
+def formexample():
+    return 'Todo...'
 
-@ask.launch
-def launch():
-    speech_text = 'Welcome to SmartCoil Alexa controller.'
-    return question(speech_text).reprompt(speech_text).simple_card(speech_text)
+@app.route('/set_speed', methods=['POST'])
+def set_speed():
+    req_data = request.get_json()
+    token = req_data['token']
+    speed = req_data['speed']
 
-@ask.intent('GpioIntent', mapping = {'status':'status'})
-def Gpio_Intent(status,room):
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(27,GPIO.OUT)
-    if status in STATUSON:
-        GPIO.output(27,GPIO.LOW)
-        return statement('turning {} the A.C.'.format(status))
-    elif status in STATUSOFF:
-        GPIO.output(27,GPIO.HIGH)
-        return statement('turning {} the A.C.'.format(status))
-    else:
-        return statement('Smartcoil can\'t process your request.')
-
-@ask.intent('AMAZON.HelpIntent')
-def help():
-    speech_text = 'You can control the SmartCoil with this app. Say things like "tell raspberry to turn on AC" or "tell raspberry to set heater on high speed."'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-
-
-@ask.session_ended
-def session_ended():
-    return "{}", 200
-
+    res = 'the speed is now {}'.format(speed)
+    print('DEBUG:',res)
+    return res
 
 if __name__ == '__main__':
-    verify = ''
-    if 'ASK_VERIFY_REQUESTS' in os.environ:
-        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
-    if verify == 'false':
-        app.config['ASK_VERIFY_REQUESTS'] = False
-
-app.run(debug=True)
+    app.run(debug=True, port=5000, host='0.0.0.0') #run app in debug mode on port 5000
