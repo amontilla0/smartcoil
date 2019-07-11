1edc953460a2c8d8794bb629ce35412be6e324d5
Abraham R. Montilla Linares <amontilla0@gmail.com>
2019-07-10 21:16:59 -0400

    - addition of simple Message class used in thread communications

--- smartcoil/utils/utils.py
+++ smartcoil/utils/utils.py
@@ -1,2 +1,8 @@
 def c_to_f(celcius):
     return celcius * 9 / 5 + 32
+
+class Message():
+    def __init__(self, type, action = None, params = None):
+        self.type = type
+        self.action = action
+        self.params = params
